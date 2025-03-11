c993c8b7fb6bc064a5de5ca588f9c1c696590ace
Martin Pravda <martin.pravda@gmail.com>
2025-03-11 14:35:27 +0100

    Feat: Register products to the main app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,29 @@
+import type {Product} from "./products-row/models";
+
 import {Component} from "@angular/core";
-import {RouterOutlet} from "@angular/router";
+import {ProductsComponent} from "./products/products.component";
+
+const mocks: Product[] = [
+  {
+    id: crypto.randomUUID(),
+    name: "Apple",
+    price: 1,
+    quantity: 1
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Banana",
+    price: 1.5,
+    quantity: 2
+  }
+];
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet],
+  imports: [ProductsComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss"
 })
 export class AppComponent {
-  title = "frontend";
+  data: Product[] = mocks;
 }
